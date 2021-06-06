96c85b7cb0499c38a3327000b00617794cd45fee
Sankalp-G <sankalp.gawali@gmail.com>
2021-06-06 20:53:39 +0530

    added buttons for several colors

--- scripts.js
+++ scripts.js
@@ -5,9 +5,12 @@ const clearButton = document.querySelector("#clear_button")
 const cellDiv = document.createElement('div')
 const cellInput = document.querySelector('#cell_input')
 const numberEnterButton = document.querySelector('#enter_button')
+const colorContainer = document.querySelector('#color_container')
+const colorButtons = colorContainer.childNodes
 cellDiv.setAttribute('id', 'grid_cell')
+cellDiv.setAttribute('style', 'background-color: rgb(255, 255, 255);')
 
-
+let brushColor = 'black';
 
 
 //function declarations
@@ -23,7 +26,7 @@ function cloneElement(element, num, appendTo){
 function clearGrid(){
     const allCells = document.querySelectorAll('#grid_cell')
     allCells.forEach((cell) => {
-        cell.setAttribute('style', 'background-color: white;')
+        cell.setAttribute('style', 'background-color: rgb(255, 255, 255);')
     })
 }
 
@@ -32,12 +35,37 @@ function setGridSize(size){
     gridContainer.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`)
 }
 
-//changing div color to black whenever the pointer enters a cell
+function randomNum(max){
+    let num = Math.floor(Math.random() * max)
+    return num
+}
+
+//changing div color whenever the pointer enters a cell
 function listenToCells(){
     const allCells = document.querySelectorAll('#grid_cell')
     allCells.forEach((cell) => {
-        cell.addEventListener('pointerenter', () => { 
-            cell.setAttribute('style', 'background-color: black;')
+        cell.addEventListener('pointerenter', () => {
+            if(brushColor == 'black'){
+                cell.setAttribute('style', 'background-color: rgb(0, 0, 0);')
+            } 
+            else if(brushColor == 'eraser'){
+                cell.setAttribute('style', 'background-color: rgb(255, 255, 255);')
+            } 
+            else if (brushColor == 'grayscale'){
+                //getting individual color values
+                let split = cell.style.backgroundColor.split(', ')
+                rgbArr = [split[0].split('rgb(')[1], split[1], split[2].split(')')[0]]
+
+                //if all colors are equal making cells 10% darker otherwise turning the cell light gray
+                if (rgbArr[0] == rgbArr[1] && rgbArr[0] == rgbArr[2]) {
+                    let val = rgbArr[0] - 25.6;
+                    cell.setAttribute('style', `background-color: rgb(${val}, ${val}, ${val});`)
+                }
+                else {cell.setAttribute('style', 'background-color: rgb(230.4, 230.4, 230.4);')}
+            }
+            else if (brushColor == 'random'){
+                cell.setAttribute('style', `background-color: rgb(${randomNum(256)}, ${randomNum(256)}, ${randomNum(256)});`)
+            }
         });
     });
 }
@@ -57,4 +85,11 @@ numberEnterButton.addEventListener('click', () => {
     setGridSize(size)
     cloneElement(cellDiv, size * size, gridContainer)
     listenToCells()
+})
+
+colorButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        brushColor = button.getAttribute('id')
+        console.log(button.getAttribute('id'))
+    })
 })
